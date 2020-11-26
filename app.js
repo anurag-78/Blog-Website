a0fb6af43d33b3be1517d579b7ca6a861aceb83f
anurag diwedi <anuragdiwedi78@gmail.com>
2020-11-26 20:16:00 +0530

    Updated Some Content

--- app.js
+++ app.js
@@ -104,6 +104,6 @@ Post.findOne({_id: requestedPostId}, function(err, post){
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.port||3000, function() {
   console.log("Server started on port 3000");
 });
